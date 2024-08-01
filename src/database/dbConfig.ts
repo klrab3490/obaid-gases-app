6e0e91ecab4a983c0cea9328bfbd70cbc736cce9
Rab Lap <rahulbushi69@gmail.com>
2024-08-01 21:17:29 +0530

    chore: Update MongoDB connection code and package dependencies

--- src/database/dbConfig.ts
+++ src/database/dbConfig.ts
@@ -1,12 +1,13 @@
-// eslint-disable-next-line no-console
 import mongoose from 'mongoose';
 
 export default async function connectToMondoDB() {
   const url = 'mongodb://127.0.0.1:27017/obaidgases';
   try {
     await mongoose.connect(url);
+    // eslint-disable-next-line no-console
     console.log('Connected to MongoDB');
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error('Error connecting to MongoDB: ', error);
     throw error;
   }
